586a0e88456ec359273ce1e1f0ea4d6b8ce5a463
kariya hiromu <hiromu.kariya@gmail.com>
2022-03-17 00:20:57 +0900

    web+db press vol.127 phoenix part 3

--- realworld/lib/realworld/blogs/article.ex
+++ realworld/lib/realworld/blogs/article.ex
@@ -1,18 +1,35 @@
 defmodule Realworld.Blogs.Article do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Realworld.Blogs.Comment
 
   schema "articles" do
-    field :body, :string
-    field :title, :string
+    field(:body, :string)
+    field(:title, :string)
+    has_many(:comments, Comment, on_delete: :delete_all)
+
+    many_to_many(:tags, Tag,
+      join_through: ArticleTag,
+      on_replace: :delete,
+      on_delete: :delete_all
+    )
 
     timestamps()
   end
 
   @doc false
-  def changeset(article, attrs) do
+  def changeset(article, attrs, tags \\ []) do
     article
     |> cast(attrs, [:title, :body])
     |> validate_required([:title, :body])
+    |> put_assoc(:tags, tags)
+  end
+
+  def list_articles do
+    Repo.all(Article) |> Repo.preload(:tags)
+  end
+
+  def get_article!(id) do
+    Repo.get!(Article, id) |> Repo.preload(:tags)
   end
 end
